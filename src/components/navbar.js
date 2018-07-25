e5302ae1ad6811eceddf34e2478caf9cdc187746
Mike <mikeramzdesign@gmail.com>
2018-07-24 20:04:47 -0700

    Logout button working!!

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,31 +1,47 @@
 import React from "react";
 //if i see line 3 its because I should be importing something
+import {connect} from 'react-redux';
 import { Link } from "react-router-dom";
 import "../comp_styles/navbar.css"
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
 
 class NavBar extends React.Component {
 
-  // logOut() {
-  //   this.props.dispatch(logout())
-  // }
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
 
   render() {
+    let logOutButton;
+    if (this.props.loggedIn) {
+      logOutButton = (
+        <button onClick={() => this.logOut()}>Log out</button>
+      );
+    }
 
-    return(
-        <div className="navContainer">
-          <Link to="/" className="headerLink">
-            <h1>mobi</h1>
-          </Link>
-          <nav>
-            <div className="navWide">
-              <Link className="navLink" to="/login">Login</Link>
-              <Link className="navLink" to="/registration-page">Signup</Link>
-            </div>
-          </nav>
+    return (
+      <div className="navContainer">
+        <Link to="/" className="headerLink">
+          <h1>mobi</h1>
+        </Link>
+        <nav>
+          <div className="navWide">
+            <Link className="navLink" to="/login">Login</Link>
+            <Link className="navLink" to="/registration-page">Signup</Link>
+            {logOutButton}
+          </div>
+        </nav>
       </div>
     )
   }
 
 }
 
-export default NavBar
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(NavBar);
+// export default NavBar
