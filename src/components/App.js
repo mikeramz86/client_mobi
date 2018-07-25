9c519c129d969eafb7a7e99cdf94d14f6f6cdaf6
Mike <mikeramzdesign@gmail.com>
2018-07-24 19:27:24 -0700

    Routes Dashboard and Login now working

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,24 @@
 
-import React from 'react';
-import {connect} from 'react-redux';
-import {Router, Route} from 'react-router-dom';
+// import React from 'react';
+// import {connect} from 'react-redux';
+// import {Router} from 'react-router-dom';
 
-import NavBar from './navbar';
-import Landing from './landing';
+// import NavBar from './navbar';
+// import Landing from './landing';
 // import Dashboard from './dashboard';
 // import RegistrationPage from './registration-page';
 
-export class App extends React.Component {
+// export class App extends React.Component {
 
-  render() {
-    return (
-      <Router>
-          <div className="App">
-            <NavBar/>
-            <Landing/>
-            {/* <Route exact path="/landing" component={Landing} />
-            <Route exact path="/dashboard" component={Dashboard} /> */}
-            {/* <Route exact path="/register" component={RegistrationPage} /> */}
-
-
-          </div>
-      </Router>
-    );
-  }
-}
-
-
-const mapStateToProps = state => ({
-  hasAuthToken: state.auth.authToken !== null,
-  loggedIn: state.auth.currentUser !== null
-});
-
-export default connect(mapStateToProps)(App)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import '../comp_styles/App.css';
-
-// import Dashboard from './dashboard';
-// import NavBar from './navbar';
-// import MAINSECTION from './mainSection';
-// // import RegistrationPage from './components/registration-page';
-// import InfoSection from './info';
-// import Footer from './footer';
-// import MainSection from './mainSection';
-
-
-
-
-
-// class App extends Component {
 //   render() {
 //     return (
 //       <Router>
 //           <div className="App">
-
 //             <NavBar/>
-
-//             <MainSection/>
-            
-//             <InfoSection/>
-
-//             <Footer/>
+//             <Landing/>
+//             <Route exact path="/landing" component={Landing} />
+//             <Route exact path="/dashboard" component={Dashboard} />
+//             <Route exact path="/register" component={RegistrationPage} />
 
 
 //           </div>
@@ -91,6 +27,78 @@ export default connect(mapStateToProps)(App)
 //   }
 // }
 
-// export default App;
+
+// const mapStateToProps = state => ({
+//   hasAuthToken: state.auth.authToken !== null,
+//   loggedIn: state.auth.currentUser !== null
+// });
+
+// export default connect(mapStateToProps)(App)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../comp_styles/App.css';
+
+import Dashboard from './dashboard';
+import NavBar from './navbar';
+import MAINSECTION from './mainSection';
+// import RegistrationPage from './components/registration-page';
+import InfoSection from './info';
+import Footer from './footer';
+import MainSection from './mainSection';
+import Landing from './landing'
+import LogIn from './login-form';
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <div className="App">
+
+            <NavBar/>
+            <Landing/>
+
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/login" component={LogIn} />
+
+
+
+          </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
+
+//Incremental Process!!!!!!!!!!!!
+// Comment out new code we had work out the part that works
+// Start out with code that works
+// add a line a time
+//if you do know what the code does maybe add a few lines
 
 
