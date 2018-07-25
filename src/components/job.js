e529915e17bbc14a6f04f6653220916913881350
Mike <mikeramzdesign@gmail.com>
2018-07-25 13:26:37 -0700

    deleted full view job details and changed it to update

--- src/components/job.js
+++ src/components/job.js
@@ -27,25 +27,6 @@ class Job extends React.Component {
   render() {
     if (!this.state.showDetails) {
         return (
-        <div className="list">
-          <div>Company:</div>
-          <div>Job:</div>
-          <div>Stage:</div>
-          <div>Status:</div>
-  
-          <button className=" loginbtn"
-            onClick={() => this.toggleShowDetails(true)}
-          >View</button>
-  
-        </div>
-        );
-    } else {
-        return (
-          <UpdateJob/>
-      );
-    }
-    
-    return (
         <div className="job">
             <div>Company:</div>
             <div>Job:</div>
@@ -55,16 +36,92 @@ class Job extends React.Component {
             <div>Comp:</div>
             <div>Pros:</div>
             <div>Cons:</div>
-            <button className=" loginbtn"
-              onClick={() => this.updateJobDetails(true)}>Update</button>
-            <button className="loginbtn" type="submit">Delete</button>
+  
+          <button className=" loginbtn"
+            onClick={() => this.toggleShowDetails(true)}
+          >Update Job</button>
+  
         </div>
-    )
+        );
+    } else {
+        return (
+          <UpdateJob/>
+      );
+    }
+    
   }
 
 }
 
 export default Job
+
+// import React from "react";
+// import "../comp_styles/job.css"
+// import UpdateJob from './updatejob';
+
+// class Job extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         showDetails: false
+//     }
+//   }
+
+//   toggleShowDetails(toggle) {
+//     this.setState({
+//         showDetails: toggle
+//     });
+//   }
+
+//   updateJobDetails(toggle){
+//     this.setState({
+//       updateDetails: toggle
+//     });
+//   }
+
+
+
+//   render() {
+//     if (!this.state.showDetails) {
+//         return (
+//         <div className="list">
+//           <div>Company:</div>
+//           <div>Job:</div>
+//           <div>Stage:</div>
+//           <div>Status:</div>
+  
+//           <button className=" loginbtn"
+//             onClick={() => this.toggleShowDetails(true)}
+//           >View</button>
+  
+//         </div>
+//         );
+//     } else {
+//         return (
+//           <UpdateJob/>
+//       );
+//     }
+    
+//     return (
+//         <div className="job">
+//             <div>Company:</div>
+//             <div>Job:</div>
+//             <div>Stage:</div>
+//             <div>Status:</div>
+//             <div>Date:</div>
+//             <div>Comp:</div>
+//             <div>Pros:</div>
+//             <div>Cons:</div>
+//             <button className=" loginbtn"
+//               onClick={() => this.updateJobDetails(true)}>Update</button>
+//             <button className="loginbtn" type="submit">Delete</button>
+//         </div>
+//     )
+//   }
+
+// }
+
+// export default Job
 
 
 
