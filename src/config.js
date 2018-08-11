3cf3ddf7005190ab5c39c16ff8e23995f114310c
Mike <mikeramzdesign@gmail.com>
2018-08-10 18:12:15 -0700

    revert

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://mighty-peak-72244.herokuapp.com/';
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
 
+    
